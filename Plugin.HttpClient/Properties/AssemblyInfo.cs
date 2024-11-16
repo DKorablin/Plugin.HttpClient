98b9ff382875973c5afef936950d295bafa2cdff
DKorablin2 <danila.korablin@unity3d.com>
2024-11-16 22:51:30 +0300

    YAML: Initial CI/CD commit

--- Plugin.HttpClient/Properties/AssemblyInfo.cs
+++ Plugin.HttpClient/Properties/AssemblyInfo.cs
@@ -3,18 +3,21 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 [assembly: Guid("fdddc14d-5911-47bf-92dd-815a383c3f27")]
+[assembly: System.CLSCompliant(false)]
+
+#if NETCOREAPP
+[assembly: AssemblyMetadata("ProjectUrl", "https://dkorablin.ru/project/Default.aspx?File=99")]
+#else
 
 [assembly: AssemblyTitle("Plugin.HttpClient")]
+[assembly: AssemblyProduct("Plugin.HttpClient")]
+[assembly: AssemblyCompany("Danila Korablin")]
+[assembly: AssemblyCopyright("Copyright © Danila Korablin 2015-2024")]
 [assembly: AssemblyDescription("HTTP Test client & server")]
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
 #else
 [assembly: AssemblyConfiguration("Release")]
 #endif
-[assembly: AssemblyCompany("Danila Korablin")]
-[assembly: AssemblyProduct("Plugin.HttpClient")]
-[assembly: AssemblyCopyright("Copyright © Danila Korablin 2015-2024")]
 
-[assembly: AssemblyVersion("1.0.3")]
-[assembly: AssemblyFileVersion("1.0.3")]
-[assembly: System.CLSCompliant(false)]
+#endif
---
+++ README.md
@@ -0,0 +1,2 @@
+# Plugin.HttpClient
+Http test client and server plugin compatible with SAL host applications
